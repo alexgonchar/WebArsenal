b6b8d83c9f0b22ecfbcf53c54ecddc31c09696de
Alexander <gonchar.profit@gmail.com>
2012-06-04 23:44:11 +0300

    - Added Design files
    - Translated "Cash On Delivery (COD)"
    - Main page slider style fixes
    - Login page style changes

---
+++ src/Presentation/Nop.Web.Framework/UI/BootstrapLayoutExt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace Nop.Web.Framework.UI
+{
+    public static class BootstrapLayoutExt
+    {
+        public static IHtmlString Row(this HtmlHelper helper, HtmlString rowContent)
+        {
+            var contentStr = rowContent.ToHtmlString();
+            if (!string.IsNullOrWhiteSpace(contentStr))
+            {
+                var sb = new StringBuilder("<div class='row-fluid show-grid'><div class='span12'>", 200 + contentStr.Length);
+                sb.Append(contentStr);
+                sb.Append("</div></div>");
+                return new HtmlString(sb.ToString());
+            }
+
+            return new HtmlString("");
+        }
+    }
+}
